ef15a89a5da8c1131ad102bb325f242b89c3db0f
Meidozuki <1025473107@qq.com>
2022-05-28 18:22:57 +0800

    update: succeed to draw a line

--- src/rasterizer.cpp
+++ src/rasterizer.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "rasterizer.hpp"
+
+#include <cmath>
+
+void drawLine_DigitalDifferentialAnalyzer(int x1,int y1,int x2,int y2,Rasterizer &r) {
+    int dx = x2 - x1, dy = y2 - y1;
+    ColorType color{1.0f,1.0f,1.0f};
+
+    int n_step = std::abs(dx) > std::abs(dy) ? std::abs(dx) : std::abs(dy);
+    float delta_x = dx / n_step , delta_y = dy / n_step;
+    float x = x1, y = y1;
+
+    for (int i=0;i < n_step;++i) {
+        x = x + delta_x;
+        y = y + delta_y;
+        r.setPixel(x,y,color);
+    }
+}
+
+void Rasterizer::drawLine(Eigen::Vector3f begin,Eigen::Vector3f end) {
+    drawLine_DigitalDifferentialAnalyzer(begin.x(),begin.y(),end.x(),end.y(),*this);
+
+}
--- src/rasterizer.hpp
+++ src/rasterizer.hpp
@@ -10,6 +10,7 @@
 #include <tuple>
 
 #include <Eigen/Core>
+#include "unsupported/Eigen/CXX11/Tensor"
 
 #define DEBUG_MODE 1
 #if DEBUG_MODE
@@ -17,30 +18,35 @@
 using std::stringstream;
 #endif
 
+typedef float DType;
 typedef Eigen::Vector3f ColorType;
+using Eigen::Tensor;
 using Eigen::Dynamic;
 
 class Rasterizer {
 private:
     int width_, height_;
-    Eigen::Matrix<ColorType, Dynamic, Dynamic> frame_buffer_;
+    Tensor<DType, 3> frame_buffer_; // 3-dim tensor
 
 public:
     Rasterizer(int h,int w): width_(w),height_(h) {
-        frame_buffer_.resize(height_,width_);
+        frame_buffer_.resize({height_, width_, 3});
     }
     Rasterizer(): Rasterizer(0,0) {};
 
+    //getters
     const int width() const {return width_;}
     const int height() const {return height_;}
-    std::tuple<int, int> get_framebuffer_shape() {return {frame_buffer_.rows(),frame_buffer_.cols()};}
+    const auto & framebuffer() {return frame_buffer_;}
+//    std::tuple<int, int> get_framebuffer_shape() {return {frame_buffer_.size(),frame_buffer_.cols()};}
 
     inline void setPixel(int x,int y,const ColorType& color);
 
-
+    void drawLine(Eigen::Vector3f begin,Eigen::Vector3f end);
 };
 
-void Rasterizer::setPixel(int x,int y,const ColorType& color) {
+
+inline void Rasterizer::setPixel(int x,int y,const ColorType& color) {
 #if DEBUG_MODE
     if (x < 0 || x > width_ ||
         y < 0 || y > height_)
@@ -56,7 +62,9 @@ void Rasterizer::setPixel(int x,int y,const ColorType& color) {
     }
 #endif
 
-    frame_buffer_(x,y)=color;
+    frame_buffer_(x,y,0)=color[0];
+    frame_buffer_(x,y,1)=color[1];
+    frame_buffer_(x,y,2)=color[2];
 }
 
 
