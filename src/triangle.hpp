4dd529b26ebaa7510aa4121e4c834f0f14ca4a44
Meidozuki <68063235+Meidozuki@users.noreply.github.com>
2022-05-31 11:49:42 +0800

    增加插值

--- src/triangle.hpp
+++ src/triangle.hpp
@@ -18,8 +18,18 @@ public:
     array<Vector3f ,3> vertex_;
     array<Vector3f ,3> color_;
 
+    Triangle();
+
     void setVertex(int idx, const Vector3f &v) {vertex_[idx] = v;}
     void setColor(int idx, const Vector3f &color) {color_[idx] = color;}
+    void setAllColors(const Vector3f &color) {
+        setColor(0,color);
+        setColor(1,color);
+        setColor(2,color);
+    }
+
+    Vector3f getColor(int idx) const {return color_[idx];}
 };
+
 
 #endif //RASTERIZER_TRIANGLE_HPP
