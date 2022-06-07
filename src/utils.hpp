28d2f56938952cc1219ce09eef742745db00dbc6
Meidozuki <1025473107@qq.com>
2022-06-08 00:11:11 +0800

    shader初始化用constant

--- src/utils.hpp
+++ src/utils.hpp
@@ -9,8 +9,14 @@
 
 #include <Eigen/Core>
 
+typedef Eigen::Vector3f ColorType;
+
 inline Eigen::Vector4f Vector3to4(const Eigen::Vector3f &v, float w = 1.0f) {
     return {v.x(), v.y(), v.z(), w};
+}
+
+inline Eigen::Vector3f FloatToVector(float f) {
+    return {f,f,f};
 }
 
 #endif //MAIN_CPP_SRC_UTILS_HPP_
