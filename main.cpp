13deed8c0ae2fa78b950b8d2e8c2d0e5695098ec
Meidozuki <68063235+Meidozuki@users.noreply.github.com>
2022-06-23 15:30:20 +0800

    更改triangle private变量，增加obj小奶牛

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@
 #include "obj_loader.hpp"
 #include "voxel/polygon.hpp"
 
-#if 1
+#if 0
   #include "debug_test_example.cpp"
 #endif
 
@@ -36,6 +36,42 @@ int main() {
 
     std::vector<Triangle> triangle_list;
 
+    ObjLoader obj_loader;
+    obj_loader.loadObjFile("../cow.obj");
+
+    auto& vertices=obj_loader.vertices_buf;
+    for (Eigen::Vector3i &index: obj_loader.indices_buf) {
+        Triangle tri;
+        for (int i=0;i < 3;++i) {
+            int idx=index[i]-1;
+            auto vert=vertices[idx];
+            tri.setVertex(i, vert);
+        }
+        float col=tanh(tri.vertex(0).z())/2+0.5;
+        col=1;
+        tri.setColor(0,{0,1,col});
+        tri.setColor(1,{0,1,col});
+        tri.setColor(2,{0,1,col});
+        tri.setNormals({});
+        triangle_list.push_back(std::move(tri));
+    }
+//    raster.draw(triangle_list);
+
+    float startX=-0.2,startY=0.2,startZ=-0.2, cube_size=0.12;
+    int n_cubes=5;
+
+    for (int i=0;i < n_cubes;++i) {
+        for (int j=0;j < n_cubes;++j) {
+            float left = startX + cube_size*2*i;
+            float bottom = startY + cube_size*2*j;
+
+            ColorType color(1.0-(1.0*i/n_cubes),i*cube_size,(1.0*i/n_cubes));
+            voxel::Mesh::setMeshColor(color);
+            voxel::Cube cube(left,bottom,startZ,left+cube_size,bottom+cube_size,startZ+cube_size);
+            cube.getTriangles(triangle_list);
+        }
+    }
+
 
     int key =0;
     while (key != 27) {
@@ -46,32 +82,15 @@ int main() {
         raster.setProjection(GetProjectionMatrix(60, 1, 0.1, 10));
 
 
+//        test_triangle();
+//        test_cone();
 
-        triangle_list.clear();
 
-        float startX=-0.2,startY=0.2,startZ=-0.2, cube_size=0.12;
-        int n_cubes=5;
-
-        for (int i=0;i < n_cubes;++i) {
-            for (int j=0;j < n_cubes;++j) {
-                float left = startX + cube_size*2*i;
-                float bottom = startY + cube_size*2*j;
-
-                ColorType color(1.0-(1.0*i/n_cubes),i*cube_size,(1.0*i/n_cubes));
-                voxel::Mesh::setMeshColor(color);
-                voxel::Cube cube(left,bottom,startZ,left+cube_size,bottom+cube_size,startZ+cube_size);
-                cube.getTriangles(triangle_list);
-            }
-        }
 
         voxel::Mesh::setMeshColor({0,0.5,0.7});
 
-        voxel::Cone cone(16,{0.5,0,0.2},0.15,1.5);
-        cone.getTriangles(triangle_list);
-        voxel::Cone cone2(16,{0.5,0,0.2},0.15,-1.5);
-        cone2.getTriangles(triangle_list);
-
         raster.draw(triangle_list);
+//        triangle_list.clear();
 
 
         cv::Mat image(height,width,CV_32FC3);
@@ -80,7 +99,7 @@ int main() {
         cv::flip(image,image,0); //竖直翻转
         cv::imshow("image",image);
 
-        key = cv::waitKey(100);
+        key = cv::waitKey(200);
         angle += 10;
     }
 
