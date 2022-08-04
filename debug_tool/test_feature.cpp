2170d554c770b4c4060606da35a71b2309c8497d
Meidozuki <1025473107@qq.com>
2022-08-04 22:05:52 +0800

    增加 viewmodel

---
+++ debug_tool/test_feature.cpp
@@ -0,0 +1,12 @@
+//
+// Created on 2022/6/23.
+//
+
+#include <iostream>
+#include <any>
+
+void f(std::any a) {}
+
+int main (void) {
+    ;
+}
---
+++ src/image_io.hpp
@@ -0,0 +1,30 @@
+//
+// Created by user on 2022/7/26.
+//
+
+#ifndef RASTERIZER_IMAGE_IO_HPP
+#define RASTERIZER_IMAGE_IO_HPP
+
+#include <filesystem>
+#include <string>
+#include <optional>
+
+#include <Eigen/Core>
+#include <opencv2/core/eigen.hpp>
+#include <opencv2/core.hpp>
+
+#include "rasterizer.hpp"
+
+namespace imageio {
+    using std::filesystem::exists;
+    using std::filesystem::create_directory;
+
+    void save_img(Rasterizer &r,std::optional<std::string> filename = std::nullopt);
+    void clear_dir(std::string path);
+
+
+    void show_img(Rasterizer &raster, const std::string &title = "image");
+    cv::Mat eigen2cv_(Rasterizer &raster);
+}
+
+#endif //RASTERIZER_IMAGE_IO_HPP
